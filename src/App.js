0dd7b7811f7ced1bdb8e5a0874f451a57b53b08f
LeoF-S <leo.fleury-stryzewski@estiam.com>
2021-11-02 09:34:43 +0100

    out of date commit : search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import { useEffect, useState } from "react";
 import PokemonThumbnail from "./components/PokemonThumbnail";
 
+import Search from './Search';
+
 
 function App() {
 
   const [allPokemons, setAllPokemons] = useState([])
   const [loadMore, setLoadMore]= useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+
+  
+  const filterPosts = (posts, query) => {
+    if (!query) {
+        return posts;
+    }
+
+    return posts.filter((post) => {
+        const postName = post.name.toLowerCase();
+        return postName.includes(query);
+    });
+};
+
+  const posts = [
+    { id: '1', name: 'Bulbasaur' },
+    { id: '2', name: 'Squirtle' },
+    { id: '3', name: 'Charmander' },
+    { id: '4', name: 'Another pokemon' },
+  ];
+
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');    
+  const filteredPosts = filterPosts(posts, searchQuery);
+
   
   const getAllPokemons = async ()=>{
     const res = await fetch(loadMore)
@@ -35,17 +63,35 @@ function App() {
   return(
     <div className="app-container">
       <h1>Pokémon Codex</h1>
+      <div>
+          <Search
+            searchQuery={searchQuery}                
+            setSearchQuery={setSearchQuery}            
+          />
+            <ul>
+              {filteredPosts.map(post => (                    
+              <li key={post.key}>{post.name}</li>
+              ))}
+            </ul>
+        </div>
+
+
+
       <div className="pokemon-container">
         <div className="all-container">
+ 
+
+
         { allPokemons.map((pokemon, index) => 
           <PokemonThumbnail 
-          id={pokemon.id}
-          name={pokemon.name}
-          image={pokemon.sprites.other.dream_world.front_default}
-          type={pokemon.types[0].type.name}
-          key={index}
+            id={pokemon.id}
+            name={pokemon.name}
+            image={pokemon.sprites.other.dream_world.front_default}
+            type={pokemon.types[0].type.name}
+            key={index}
           />
         )}
+
         </div>
         <button className ="load-more" onClick={() => getAllPokemons()}>Load more</button>
       </div>
